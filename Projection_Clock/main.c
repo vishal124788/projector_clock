b66412fad5f5a89a0593365754b11748c969d8c4
Vishal <Vishal@OfficePC.mshome.net>
2016-01-08 13:19:33 +0530

    First Commit

---
+++ Projection_Clock/main.c
@@ -0,0 +1,18 @@
+/*
+ *  ======= main ========
+ *
+ *  Created on: Jan 8, 2016
+ *  Author:     Vishal
+ */
+#include <xdc/runtime/System.h>
+
+/*
+ * ======== main ========
+ */
+Int main(void)
+{
+	int i=0;
+    System_printf("hello world.\n");
+
+    return (0);
+}
