62268bba2ba8a728cb5c6d030093177689de14a7
Vishal <Vishal@OfficePC.mshome.net>
2016-01-11 10:35:25 +0530

    second commit

---
+++ Projection_Clock/.config/xconfig_Projection_Clock/package/package.defs.h
@@ -0,0 +1,13 @@
+/*
+ *  Do not modify this file; it is automatically 
+ *  generated and any modifications will be overwritten.
+ *
+ * @(#) xdc-A71
+ */
+
+#ifndef xconfig_Projection_Clock__
+#define xconfig_Projection_Clock__
+
+
+
+#endif /* xconfig_Projection_Clock__ */ 
---
+++ Projection_Clock/Debug/main.pp
@@ -0,0 +1,159 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: C:/ti/xdctools_3_31_01_33_core/packages/xdc/runtime/System.h
+main.obj: C:/ti/xdctools_3_31_01_33_core/packages/xdc/std.h
+main.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.6/include/stdarg.h
+main.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.6/include/stddef.h
+main.obj: C:/ti/tirtos_tivac_2_14_04_31/products/bios_6_42_03_35/packages/ti/targets/arm/elf/std.h
+main.obj: C:/ti/tirtos_tivac_2_14_04_31/products/bios_6_42_03_35/packages/ti/targets/arm/elf/M4F.h
+main.obj: C:/ti/tirtos_tivac_2_14_04_31/products/bios_6_42_03_35/packages/ti/targets/std.h
+main.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.6/include/stdint.h
+main.obj: C:/ti/xdctools_3_31_01_33_core/packages/xdc/runtime/xdc.h
+main.obj: C:/ti/xdctools_3_31_01_33_core/packages/xdc/runtime/Types.h
+main.obj: C:/ti/xdctools_3_31_01_33_core/packages/xdc/runtime/Types__prologue.h
+main.obj: C:/ti/xdctools_3_31_01_33_core/packages/xdc/runtime/package/package.defs.h
+main.obj: C:/ti/xdctools_3_31_01_33_core/packages/xdc/runtime/Types__epilogue.h
+main.obj: C:/ti/xdctools_3_31_01_33_core/packages/xdc/runtime/IModule.h
+main.obj: C:/ti/xdctools_3_31_01_33_core/packages/xdc/runtime/Types.h
+main.obj: C:/ti/xdctools_3_31_01_33_core/packages/xdc/runtime/Types.h
+main.obj: C:/ti/xdctools_3_31_01_33_core/packages/xdc/runtime/IHeap.h
+main.obj: C:/ti/xdctools_3_31_01_33_core/packages/xdc/runtime/Types.h
+main.obj: C:/ti/xdctools_3_31_01_33_core/packages/xdc/runtime/IInstance.h
+main.obj: C:/ti/xdctools_3_31_01_33_core/packages/xdc/runtime/Types.h
+main.obj: C:/ti/xdctools_3_31_01_33_core/packages/xdc/runtime/IModule.h
+main.obj: C:/ti/xdctools_3_31_01_33_core/packages/xdc/runtime/Error.h
+main.obj: C:/ti/xdctools_3_31_01_33_core/packages/xdc/runtime/Types.h
+main.obj: C:/ti/xdctools_3_31_01_33_core/packages/xdc/runtime/Error__prologue.h
+main.obj: C:/ti/xdctools_3_31_01_33_core/packages/xdc/runtime/Main.h
+main.obj: C:/ti/xdctools_3_31_01_33_core/packages/xdc/runtime/Types.h
+main.obj: C:/ti/xdctools_3_31_01_33_core/packages/xdc/runtime/IModule.h
+main.obj: C:/ti/xdctools_3_31_01_33_core/packages/xdc/runtime/IGateProvider.h
+main.obj: C:/ti/xdctools_3_31_01_33_core/packages/xdc/runtime/Types.h
+main.obj: C:/ti/xdctools_3_31_01_33_core/packages/xdc/runtime/IInstance.h
+main.obj: C:/ti/xdctools_3_31_01_33_core/packages/xdc/runtime/IModule.h
+main.obj: C:/ti/xdctools_3_31_01_33_core/packages/xdc/runtime/package/Main_Module_GateProxy.h
+main.obj: C:/ti/xdctools_3_31_01_33_core/packages/xdc/runtime/Types.h
+main.obj: C:/ti/xdctools_3_31_01_33_core/packages/xdc/runtime/IInstance.h
+main.obj: C:/ti/xdctools_3_31_01_33_core/packages/xdc/runtime/IGateProvider.h
+main.obj: C:/ti/xdctools_3_31_01_33_core/packages/xdc/runtime/IModule.h
+main.obj: C:/ti/xdctools_3_31_01_33_core/packages/xdc/runtime/Types.h
+main.obj: C:/ti/xdctools_3_31_01_33_core/packages/xdc/runtime/Error__epilogue.h
+main.obj: C:/ti/xdctools_3_31_01_33_core/packages/xdc/runtime/Memory.h
+main.obj: C:/ti/xdctools_3_31_01_33_core/packages/xdc/runtime/Types.h
+main.obj: C:/ti/xdctools_3_31_01_33_core/packages/xdc/runtime/IModule.h
+main.obj: C:/ti/xdctools_3_31_01_33_core/packages/xdc/runtime/IHeap.h
+main.obj: C:/ti/xdctools_3_31_01_33_core/packages/xdc/runtime/Error.h
+main.obj: C:/ti/xdctools_3_31_01_33_core/packages/xdc/runtime/package/Memory_HeapProxy.h
+main.obj: C:/ti/xdctools_3_31_01_33_core/packages/xdc/runtime/Types.h
+main.obj: C:/ti/xdctools_3_31_01_33_core/packages/xdc/runtime/IInstance.h
+main.obj: C:/ti/xdctools_3_31_01_33_core/packages/xdc/runtime/IHeap.h
+main.obj: C:/ti/xdctools_3_31_01_33_core/packages/xdc/runtime/Error.h
+main.obj: C:/ti/xdctools_3_31_01_33_core/packages/xdc/runtime/Assert.h
+main.obj: C:/ti/xdctools_3_31_01_33_core/packages/xdc/runtime/Types.h
+main.obj: C:/ti/xdctools_3_31_01_33_core/packages/xdc/runtime/Assert__prologue.h
+main.obj: C:/ti/xdctools_3_31_01_33_core/packages/xdc/runtime/Main.h
+main.obj: C:/ti/xdctools_3_31_01_33_core/packages/xdc/runtime/Diags.h
+main.obj: C:/ti/xdctools_3_31_01_33_core/packages/xdc/runtime/Types.h
+main.obj: C:/ti/xdctools_3_31_01_33_core/packages/xdc/runtime/Diags__prologue.h
+main.obj: C:/ti/xdctools_3_31_01_33_core/packages/xdc/runtime/Main.h
+main.obj: C:/ti/xdctools_3_31_01_33_core/packages/xdc/runtime/IModule.h
+main.obj: C:/ti/xdctools_3_31_01_33_core/packages/xdc/runtime/Types.h
+main.obj: C:/ti/xdctools_3_31_01_33_core/packages/xdc/runtime/Diags__epilogue.h
+main.obj: C:/ti/xdctools_3_31_01_33_core/packages/xdc/runtime/IModule.h
+main.obj: C:/ti/xdctools_3_31_01_33_core/packages/xdc/runtime/Diags.h
+main.obj: C:/ti/xdctools_3_31_01_33_core/packages/xdc/runtime/Error.h
+main.obj: C:/ti/xdctools_3_31_01_33_core/packages/xdc/runtime/Types.h
+main.obj: C:/ti/xdctools_3_31_01_33_core/packages/xdc/runtime/Assert__epilogue.h
+main.obj: C:/ti/xdctools_3_31_01_33_core/packages/xdc/runtime/ISystemSupport.h
+main.obj: C:/ti/xdctools_3_31_01_33_core/packages/xdc/runtime/Types.h
+main.obj: C:/ti/xdctools_3_31_01_33_core/packages/xdc/runtime/IModule.h
+main.obj: C:/ti/xdctools_3_31_01_33_core/packages/xdc/runtime/IGateProvider.h
+main.obj: C:/ti/xdctools_3_31_01_33_core/packages/xdc/runtime/package/System_SupportProxy.h
+main.obj: C:/ti/xdctools_3_31_01_33_core/packages/xdc/runtime/Types.h
+main.obj: C:/ti/xdctools_3_31_01_33_core/packages/xdc/runtime/ISystemSupport.h
+main.obj: C:/ti/xdctools_3_31_01_33_core/packages/xdc/runtime/package/System_Module_GateProxy.h
+main.obj: C:/ti/xdctools_3_31_01_33_core/packages/xdc/runtime/Types.h
+main.obj: C:/ti/xdctools_3_31_01_33_core/packages/xdc/runtime/IInstance.h
+main.obj: C:/ti/xdctools_3_31_01_33_core/packages/xdc/runtime/IGateProvider.h
+main.obj: C:/ti/xdctools_3_31_01_33_core/packages/xdc/runtime/package/System_SupportProxy.h
+main.obj: C:/ti/xdctools_3_31_01_33_core/packages/xdc/runtime/package/System_Module_GateProxy.h
+
+../main.c: 
+C:/ti/xdctools_3_31_01_33_core/packages/xdc/runtime/System.h: 
+C:/ti/xdctools_3_31_01_33_core/packages/xdc/std.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.6/include/stdarg.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.6/include/stddef.h: 
+C:/ti/tirtos_tivac_2_14_04_31/products/bios_6_42_03_35/packages/ti/targets/arm/elf/std.h: 
+C:/ti/tirtos_tivac_2_14_04_31/products/bios_6_42_03_35/packages/ti/targets/arm/elf/M4F.h: 
+C:/ti/tirtos_tivac_2_14_04_31/products/bios_6_42_03_35/packages/ti/targets/std.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.6/include/stdint.h: 
+C:/ti/xdctools_3_31_01_33_core/packages/xdc/runtime/xdc.h: 
+C:/ti/xdctools_3_31_01_33_core/packages/xdc/runtime/Types.h: 
+C:/ti/xdctools_3_31_01_33_core/packages/xdc/runtime/Types__prologue.h: 
+C:/ti/xdctools_3_31_01_33_core/packages/xdc/runtime/package/package.defs.h: 
+C:/ti/xdctools_3_31_01_33_core/packages/xdc/runtime/Types__epilogue.h: 
+C:/ti/xdctools_3_31_01_33_core/packages/xdc/runtime/IModule.h: 
+C:/ti/xdctools_3_31_01_33_core/packages/xdc/runtime/Types.h: 
+C:/ti/xdctools_3_31_01_33_core/packages/xdc/runtime/Types.h: 
+C:/ti/xdctools_3_31_01_33_core/packages/xdc/runtime/IHeap.h: 
+C:/ti/xdctools_3_31_01_33_core/packages/xdc/runtime/Types.h: 
+C:/ti/xdctools_3_31_01_33_core/packages/xdc/runtime/IInstance.h: 
+C:/ti/xdctools_3_31_01_33_core/packages/xdc/runtime/Types.h: 
+C:/ti/xdctools_3_31_01_33_core/packages/xdc/runtime/IModule.h: 
+C:/ti/xdctools_3_31_01_33_core/packages/xdc/runtime/Error.h: 
+C:/ti/xdctools_3_31_01_33_core/packages/xdc/runtime/Types.h: 
+C:/ti/xdctools_3_31_01_33_core/packages/xdc/runtime/Error__prologue.h: 
+C:/ti/xdctools_3_31_01_33_core/packages/xdc/runtime/Main.h: 
+C:/ti/xdctools_3_31_01_33_core/packages/xdc/runtime/Types.h: 
+C:/ti/xdctools_3_31_01_33_core/packages/xdc/runtime/IModule.h: 
+C:/ti/xdctools_3_31_01_33_core/packages/xdc/runtime/IGateProvider.h: 
+C:/ti/xdctools_3_31_01_33_core/packages/xdc/runtime/Types.h: 
+C:/ti/xdctools_3_31_01_33_core/packages/xdc/runtime/IInstance.h: 
+C:/ti/xdctools_3_31_01_33_core/packages/xdc/runtime/IModule.h: 
+C:/ti/xdctools_3_31_01_33_core/packages/xdc/runtime/package/Main_Module_GateProxy.h: 
+C:/ti/xdctools_3_31_01_33_core/packages/xdc/runtime/Types.h: 
+C:/ti/xdctools_3_31_01_33_core/packages/xdc/runtime/IInstance.h: 
+C:/ti/xdctools_3_31_01_33_core/packages/xdc/runtime/IGateProvider.h: 
+C:/ti/xdctools_3_31_01_33_core/packages/xdc/runtime/IModule.h: 
+C:/ti/xdctools_3_31_01_33_core/packages/xdc/runtime/Types.h: 
+C:/ti/xdctools_3_31_01_33_core/packages/xdc/runtime/Error__epilogue.h: 
+C:/ti/xdctools_3_31_01_33_core/packages/xdc/runtime/Memory.h: 
+C:/ti/xdctools_3_31_01_33_core/packages/xdc/runtime/Types.h: 
+C:/ti/xdctools_3_31_01_33_core/packages/xdc/runtime/IModule.h: 
+C:/ti/xdctools_3_31_01_33_core/packages/xdc/runtime/IHeap.h: 
+C:/ti/xdctools_3_31_01_33_core/packages/xdc/runtime/Error.h: 
+C:/ti/xdctools_3_31_01_33_core/packages/xdc/runtime/package/Memory_HeapProxy.h: 
+C:/ti/xdctools_3_31_01_33_core/packages/xdc/runtime/Types.h: 
+C:/ti/xdctools_3_31_01_33_core/packages/xdc/runtime/IInstance.h: 
+C:/ti/xdctools_3_31_01_33_core/packages/xdc/runtime/IHeap.h: 
+C:/ti/xdctools_3_31_01_33_core/packages/xdc/runtime/Error.h: 
+C:/ti/xdctools_3_31_01_33_core/packages/xdc/runtime/Assert.h: 
+C:/ti/xdctools_3_31_01_33_core/packages/xdc/runtime/Types.h: 
+C:/ti/xdctools_3_31_01_33_core/packages/xdc/runtime/Assert__prologue.h: 
+C:/ti/xdctools_3_31_01_33_core/packages/xdc/runtime/Main.h: 
+C:/ti/xdctools_3_31_01_33_core/packages/xdc/runtime/Diags.h: 
+C:/ti/xdctools_3_31_01_33_core/packages/xdc/runtime/Types.h: 
+C:/ti/xdctools_3_31_01_33_core/packages/xdc/runtime/Diags__prologue.h: 
+C:/ti/xdctools_3_31_01_33_core/packages/xdc/runtime/Main.h: 
+C:/ti/xdctools_3_31_01_33_core/packages/xdc/runtime/IModule.h: 
+C:/ti/xdctools_3_31_01_33_core/packages/xdc/runtime/Types.h: 
+C:/ti/xdctools_3_31_01_33_core/packages/xdc/runtime/Diags__epilogue.h: 
+C:/ti/xdctools_3_31_01_33_core/packages/xdc/runtime/IModule.h: 
+C:/ti/xdctools_3_31_01_33_core/packages/xdc/runtime/Diags.h: 
+C:/ti/xdctools_3_31_01_33_core/packages/xdc/runtime/Error.h: 
+C:/ti/xdctools_3_31_01_33_core/packages/xdc/runtime/Types.h: 
+C:/ti/xdctools_3_31_01_33_core/packages/xdc/runtime/Assert__epilogue.h: 
+C:/ti/xdctools_3_31_01_33_core/packages/xdc/runtime/ISystemSupport.h: 
+C:/ti/xdctools_3_31_01_33_core/packages/xdc/runtime/Types.h: 
+C:/ti/xdctools_3_31_01_33_core/packages/xdc/runtime/IModule.h: 
+C:/ti/xdctools_3_31_01_33_core/packages/xdc/runtime/IGateProvider.h: 
+C:/ti/xdctools_3_31_01_33_core/packages/xdc/runtime/package/System_SupportProxy.h: 
+C:/ti/xdctools_3_31_01_33_core/packages/xdc/runtime/Types.h: 
+C:/ti/xdctools_3_31_01_33_core/packages/xdc/runtime/ISystemSupport.h: 
+C:/ti/xdctools_3_31_01_33_core/packages/xdc/runtime/package/System_Module_GateProxy.h: 
+C:/ti/xdctools_3_31_01_33_core/packages/xdc/runtime/Types.h: 
+C:/ti/xdctools_3_31_01_33_core/packages/xdc/runtime/IInstance.h: 
+C:/ti/xdctools_3_31_01_33_core/packages/xdc/runtime/IGateProvider.h: 
+C:/ti/xdctools_3_31_01_33_core/packages/xdc/runtime/package/System_SupportProxy.h: 
+C:/ti/xdctools_3_31_01_33_core/packages/xdc/runtime/package/System_Module_GateProxy.h: 
